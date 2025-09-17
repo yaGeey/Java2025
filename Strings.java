07ff0ec5974b6194d339b333bd5428586fbcc1d6
yaGeey <krabadaj@gmail.com>
2025-09-17 16:17:34 +0300

    readme + fix

--- Strings.java
+++ Strings.java
@@ -3,7 +3,7 @@ public class Strings {
    private static final char[] consonants = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
 
    public static void main(String[] args) {
-      StringBuffer s = new StringBuffer("Cat dog sun tree bird fox car ant");
+      StringBuffer s = new StringBuffer("Cat dog sun tree bird fox ant car");
       System.out.println("Original string: " + s);
 
       int targetLength = 3;
@@ -12,11 +12,29 @@ public class Strings {
    }
    
    public static StringBuffer removeConsonantWordsOfLength(StringBuffer s, int length) {
-      StringBuffer result = new StringBuffer();
-      String[] words = s.toString().split(" ");
-      for (String word : words) {
-         if (word.length() != length || !isConsonant(word.charAt(0))) {
-            result.append(word).append(" ");
+      StringBuffer result = new StringBuffer(); // result string
+      StringBuffer currentWord = new StringBuffer(); // current word temp buffer
+
+      for (int i = 0; i < s.length(); i++) {
+         char c = s.charAt(i); // checking symbol by symbol
+         if (c == ' ') {
+            // if space - end of the word
+            if (currentWord.length() != length || !isConsonant(currentWord.charAt(0))) {
+               // adding word to result if satisfies constrains...
+               result.append(currentWord).append(" ");
+            }
+            // ...and cleaning cur word
+            currentWord.setLength(0);
+         } else {
+            // if not space - adding char to cur word
+            currentWord.append(c);
+         }
+      }
+
+      // processing last word if it exists (if there is no space after it)
+      if (currentWord.length() > 0) {
+         if (currentWord.length() != length || !isConsonant(currentWord.charAt(0))) {
+            result.append(currentWord).append(" ");
          }
       }
       return result;
