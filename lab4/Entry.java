b54f2545cdcc80bd15de2d9563dc0849a87247bf
Ленченко Ярослав Юрійович <94066930+yaGeey@users.noreply.github.com>
2025-11-11 21:10:05 +0200

    Lab4 (#3)
    * f
    
    * f

---
+++ lab4/Entry.java
@@ -0,0 +1,6 @@
+package lab4;
+public class Entry {
+   public static void main(String[] args) {
+      System.out.println("Hello, World!");
+   }
+}
---
+++ lab4/Letter.java
@@ -0,0 +1,12 @@
+package lab4;
+public class Letter {
+   char value;
+
+   Letter(char value) {
+      this.value = value;
+   }
+   
+   public boolean isConsonant() {
+      return "bcdfghjklmnpqrstvwxyz".indexOf(Character.toLowerCase(value)) != -1;
+   }
+}
