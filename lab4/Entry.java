61cdcf4fa581e7c5fce0dcb83e14695bfe2b50ef
yaGeey <krabadaj@gmail.com>
2025-11-11 21:02:50 +0200

    f

--- lab4/Entry.java
+++ lab4/Entry.java
@@ -1,3 +1,4 @@
+package lab4;
 public class Entry {
    public static void main(String[] args) {
       System.out.println("Hello, World!");
--- lab4/Letter.java
+++ lab4/Letter.java
@@ -1,3 +1,4 @@
+package lab4;
 public class Letter {
    char value;
 
